b707744c5e8f273abc3e1ba48f5bd2ec9af836db
Steven A Lowe <slowe@thoughtworks.com>
2016-02-04 17:02:21 -0800

    added files for step 6

--- src/GenerateFlatRecordForShopEvent.java
+++ src/GenerateFlatRecordForShopEvent.java
@@ -3,17 +3,7 @@
  */
 public class GenerateFlatRecordForShopEvent {
 
-    public String generateFlatFormatRecordForShopEvent(Object obj) {
-        String flatFormatRecord = null;
-
-        if (obj instanceof ShopEvent) {
-            flatFormatRecord = convertShopOpenEventToFlatRecordFormat((ShopEvent) obj);
-        }
-
-        return flatFormatRecord;
-    }
-
-    private String convertShopOpenEventToFlatRecordFormat(ShopEvent shopEvent) {
+    public String generateFlatFormatRecordForShopEvent(ShopEvent shopEvent) {
         return shopEvent.convertToFlatFormat();
     }
 
